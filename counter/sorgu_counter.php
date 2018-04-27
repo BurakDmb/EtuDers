665cba84d1a70c933e9676d14252e0515d6e2daf
BurakDmb <burakhan.demirbilek@gmail.com>
2018-04-27 03:22:31 +0300

    Sorgu ve ziyaretçi sayaçları daha düzgün çalışması sağlandı, artık önceki dönemin sorgu sayılarını da göstereceğimiz için birkaç değişiklik yapıldı.
    Signed-off-by: BurakDmb <burakhan.demirbilek@gmail.com>

--- counter/sorgu_counter.php
+++ counter/sorgu_counter.php
@@ -1,18 +1,29 @@
 <?php
 function sorguCounter($durum){
     $sorgu_name = __DIR__."/sorgu.txt";
+    $eski_sorgu_name = __DIR__."/eski_sorgu.txt";
     if (!file_exists($sorgu_name)) {
         $fb = fopen($sorgu_name, "w");
         fwrite($fb,"0");
         fclose($fb);
     }
+    if (!file_exists($eski_sorgu_name)) {
+        $fb_eski = fopen($eski_sorgu_name, "w");
+        fwrite($fb_eski,"0");
+        fclose($fb_eski);
+    }
     $f2 = fopen($sorgu_name,"r");
+    $f3 = fopen($eski_sorgu_name,"r");
     $sorguVal = fread($f2, filesize($sorgu_name));
+    $eskiSorguVal = fread($f3, filesize($eski_sorgu_name));
     fclose($f2);
     if($durum==0){
         return $sorguVal;
     }
-    else{
+    else if($durum==2){
+        return $eskiSorguVal;
+    }
+    else if($durum==1){
 
         $sorguVal++;
         $f2 = fopen($sorgu_name, "w");
--- counter/ziyaretci_counter.php
+++ counter/ziyaretci_counter.php
@@ -1,30 +1,32 @@
 <?php
-session_start();
-$counter_name = "counter.txt";
+function ziyaretciCounter($durum)
+{
+    $counter_name = __DIR__."/counter.txt";
 
-// Check if a text file exists. If not create one and initialize it to zero.
-if (!file_exists($counter_name)) {
-    $fa = fopen($counter_name, "w");
-    fwrite($fa,"0");
-    fclose($fa);
+    if (!file_exists($counter_name)) {
+        $fa = fopen($counter_name, "w");
+        fwrite($fa, "0");
+        fclose($fa);
+    }
+
+    $f1 = fopen($counter_name, "r");
+    $counterVal = fread($f1, filesize($counter_name));
+    fclose($f1);
+
+
+    if (!isset($_COOKIE["ziyaret"])) {
+        setcookie("ziyaret", "evet", time() + 3600 * 24 * 30, '/');  /* 30günlük geçerli */
+
+
+        $counterVal++;
+        $f = fopen($counter_name, "w");
+        fwrite($f, $counterVal);
+        fclose($f);
+    }
+
+
+
+//echo "Tekil ziyaretçi sayısı: $counterVal ";
 }
 
-// Read the current value of our counter file
-$f1 = fopen($counter_name,"r");
-$counterVal = fread($f1, filesize($counter_name));
-fclose($f1);
 
-// Has visitor been counted in this session?
-// If not, increase counter value by one
-if(!isset($_COOKIE['ziyaret'])){
-
-    setcookie('ziyaret', 'evet',time()+(10*365*24*60*60));
-    $_COOKIE['ziyaret'] = 'evet';
-
-    $_SESSION['ziyaret']="evet";
-    $counterVal++;
-    $f = fopen($counter_name, "w");
-    fwrite($f, $counterVal);
-    fclose($f);
-}
-echo "Tekil ziyaretçi sayısı: $counterVal ";
