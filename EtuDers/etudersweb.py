9990954791746870c12725dcdf5246f4a50b6a7b
BurakDmb <burakhan.demirbilek@gmail.com>
2019-01-25 19:48:25 +0300

    Changed the api method from post to get requests to avoid CORS problems

--- EtuDers/etudersweb.py
+++ EtuDers/etudersweb.py
@@ -12,8 +12,8 @@ application=Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-from etudersUtil import Ders, dersListesiAl
-from etuders import programOlustur, cakismali, ogrenciDersProgramiGetir
+from etudersUtil import dersListesiAl
+from etuders import programOlustur, ogrenciDersProgramiGetir
 
 
 
@@ -24,8 +24,6 @@ def mainPage():
 
 @application.route('/programhesapla/', methods=['POST'])
 def programhesapla():
-    # request.form.keys()[0]
-
     derslistesi=request.form.getlist('derslistesi[]')
     cakisma=request.form.get('cakisma')
     cakismalimit=cakisma
@@ -34,42 +32,13 @@ def programhesapla():
 
     ipadres=request.headers.get('CF-Connecting-IP', request.remote_addr)
     programlar=programOlustur(derslistesi, cakismalimit, ipadres)
-    # cakismaResult=cakismali(programlar,cakismalimit)
     return Response(json.dumps([sonuc.__dict__ for sonuc in programlar], default=str))
 
 
-@application.route('/programsorgula/', methods=['POST'])
-def programsorgula():
-    ogrenciNo=request.form.get('okul_numarasi')
+@application.route('/programsorgula/<ogrenciNo>', methods=['GET'])
+def programsorgula(ogrenciNo):
     ipadres=request.headers.get('CF-Connecting-IP', request.remote_addr)
     programlar=ogrenciDersProgramiGetir(ogrenciNo, ipadres)
-    # print(json.dumps(programlar.__dict__, default=str))
-    return Response(json.dumps(programlar.__dict__, default=str))
-
-
-
-@application.route("/test1")
-def hello1():
-    # tmpders=Ders(31970,0)
-    # print(tmpders.dersAdi, tmpders.derskodu)
-    # return tmpders.dersAdi+', '+tmpders.derskodu
-    dersler=[Ders(31970, 0), Ders(40281, 0)]
-    programlar=programOlustur(dersler, 2, "")
-    sonuclar=cakismali(programlar, 2)
-    return Response(json.dumps([sonuc.__dict__ for sonuc in sonuclar], default=str), mimetype='application/json')
-
-
-
-@application.route("/test2")
-def hello2():
-    # fetchAndUpdateDB()
-    return "done"
-
-
-@application.route("/test3")
-def hello3():
-    programlar=ogrenciDersProgramiGetir("151201022", "")
-
     return Response(json.dumps(programlar.__dict__, default=str))
 
 
